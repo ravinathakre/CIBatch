870fcde250095b7438821ef780ee19b166beeb0e
rathakre <rathakre@DIN51003303.corp.capgemini.com>
2017-02-09 12:44:15 +0530

    fact tdd

---
+++ Fact_TDD/src/Maths.java
@@ -0,0 +1,19 @@
+
+public class Maths {
+	
+	public  int getFact(int num)
+	{
+		if(num<0)
+		{
+			throw new IllegalArgumentException();
+		}
+		if(num==0)
+		{
+			return 1;
+		}
+		
+		return 1;
+		
+	}
+
+}
